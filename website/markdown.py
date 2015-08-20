d2a2c4934027ce85acdf166451f32701f2a4715d
Fabian Köhler <fkoehler1024@googlemail.com>
2015-08-20 23:44:16 +0200

    add method to extract metadata

--- website/markdown.py
+++ website/markdown.py
@@ -1,7 +1,29 @@
 import mistune
+import re
 import pygments
 import pygments.formatters
 import pygments.lexers
+
+
+regex_meta_block = re.compile(r"---+\n(?P<data>(?:.*\n)*)---+", re.MULTILINE)
+regex_meta_line = re.compile(r"(?P<key>[^\:]+)\s*\:\s*(?P<value>.+)")
+
+
+def extract_meta_data(markdown):
+    m = regex_meta_block.match(markdown)
+    if not m:
+        return (None, markdown)
+    data = m.groupdict()["data"].splitlines()
+    markdown = markdown[len(m.group(0)):]
+
+    meta = {}
+    for l in data:
+        m = regex_meta_line.match(l)
+        if not m:
+            # TODO: raise exception?
+            continue
+        meta[m.groupdict()["key"]] = m.groupdict()["value"]
+    return (meta, markdown)
 
 
 class Renderer(mistune.Renderer):
