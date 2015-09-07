a789180e8bc14b610698cdc87b42d53af322ae4f
Fabian Köhler <fkoehler1024@googlemail.com>
2015-09-07 22:43:08 +0200

    nav.css and rss template

--- website/config.py
+++ website/config.py
@@ -1,9 +1,10 @@
 build_dir = "build"
-css_files = ["normalize.css", "code.css", "page.css"]
+css_files = ["normalize.css", "code.css", "links.css", "nav.css"]
 extra_files = ["index.md", "404.md"]
 preview_port = 8888
 search_paths = ["computer"]
 
 menu_items = [
+    ("/", "Home"),
     ("/computer", "Computer"),
 ]
