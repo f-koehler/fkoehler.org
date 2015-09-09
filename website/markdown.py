097b112cf9dfbd3ff816d7ebc4726ae42856cb19
Fabian Köhler <fkoehler1024@googlemail.com>
2015-09-09 21:44:54 +0200

    move code rendering into a mixin

--- website/markdown.py
+++ website/markdown.py
@@ -11,14 +11,16 @@ regex_meta_block = re.compile(r"---+\n(?P<data>(?:.*\n)*)---+", re.MULTILINE)
 regex_meta_line = re.compile(r"(?P<key>[^\:]+)\s*\:\s*(?P<value>.+)")
 
 
-class Renderer(mistune.Renderer):
+class CodeRendererMixin(object):
     def block_code(self, code, lang):
         if not lang:
-            return "\n<pre><code>{}</pre></code>".format(mistune.excape(code))
+            return "\n<pre><code>{}</pre></code>".format(mistune.excape(code.strip()))
         lexer = pygments.lexers.get_lexer_by_name(lang)
         formatter = pygments.formatters.HtmlFormatter()
         return pygments.highlight(code, lexer, formatter)
 
+
+class Renderer(CodeRendererMixin, mistune.Renderer):
     def extract_meta_data(self, markdown):
         m = regex_meta_block.match(markdown)
         if not m:
