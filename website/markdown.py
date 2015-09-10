088040726739f742a0cd7eaa2f4412a2253d0742
Fabian Köhler <fkoehler1024@googlemail.com>
2015-09-10 22:28:08 +0200

    linenos option for markdown

--- website/markdown.py
+++ website/markdown.py
@@ -1,8 +1,8 @@
 import mistune
+import re
 import pygments
 import pygments.formatters
 import pygments.lexers
-import re
 
 import website.job
 
@@ -11,13 +11,29 @@ regex_meta_block = re.compile(r"---+\n(?P<data>(?:.*\n)*)---+", re.MULTILINE)
 regex_meta_line = re.compile(r"(?P<key>[^\:]+)\s*\:\s*(?P<value>.+)")
 
 
+class InlineLexer(mistune.InlineLexer):
+    pass
+
+
+class BlockLexer(mistune.BlockLexer):
+    pass
+
+
 class CodeRendererMixin(object):
     def block_code(self, code, lang):
         if not lang:
-            return "\n<pre><code>{}</pre></code>".format(mistune.excape(code.strip()))
-        lexer = pygments.lexers.get_lexer_by_name(lang)
-        formatter = pygments.formatters.HtmlFormatter()
-        return pygments.highlight(code, lexer, formatter)
+            code = code.strip()
+            return "\n<pre><code>{}</pre></code>".format(mistune.escape(code))
+
+        linenos = self.options.get("linenos")
+
+        try:
+            lexer = pygments.lexers.get_lexer_by_name(lang, stripall=True)
+            formatter = pygments.formatters.HtmlFormatter(linenos=linenos)
+            return pygments.highlight(code, lexer, formatter)
+        except:
+            code = code.strip()
+            return "\n<pre><code>{}</pre></code>".format(mistune.excape(code))
 
 
 class Renderer(CodeRendererMixin, mistune.Renderer):
@@ -39,10 +55,12 @@ class Renderer(CodeRendererMixin, mistune.Renderer):
         return (meta, markdown)
 
 
-def pygments_css(style):
-    formatter = pygments.formatters.HtmlFormatter(style=style)
-    return formatter.get_style_defs(".highlight")
+# def pygments_css(style):
+#     formatter = pygments.formatters.HtmlFormatter(style=style)
+#     return formatter.get_style_defs(".highlight")
 
 
 renderer = Renderer()
-markdown = mistune.Markdown(renderer=renderer)
+inline_lexer = InlineLexer(renderer)
+block_lexer = BlockLexer(renderer)
+markdown = mistune.Markdown(renderer, inline=inline_lexer)
