8b9d166162304f40101ab34888be7a6da94cd5f4
Fabian Köhler <fkoehler1024@googlemail.com>
2015-09-21 23:00:36 +0200

    Add tikz job

---
+++ website/job/tikz.py
@@ -0,0 +1,32 @@
+import os.path
+import hashlib
+import subprocess
+import shutil
+import website.job.basic
+
+
+backend = "lualatex"
+
+
+class TikzJob(website.job.FileJob):
+    def run(self):
+        source = self.sources[0]
+        if not os.path.exists(source):
+            # TODO: fail
+            pass
+        sha = hashlib.sha256(self.destination).hexdigest()
+        tmpdir = os.path.join("/tmp", sha)
+        os.mkdir(tmpdir)
+        subprocess.check_output([
+            backend,
+            "--interaction=errorstopmode",
+            "--output-directory="+tmpdir,
+            source,
+        ])
+        pdffile = os.path.join(tmpdir, os.path.basename(source))
+        subprocess.check_output([
+            "pdf2svg",
+            pdffile,
+            self.destination
+        ])
+        shutil.rmtree(tmpdir)
